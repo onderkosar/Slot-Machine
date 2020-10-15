57699640adca28307824b4d3c1d4e2b8f798c30b
Önder Koşar <onderkosar@hotmail.com>
2020-10-15 23:30:00 +0300

    resource files are added into the project

---
+++ Slot Machine/App/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Önder Koşar on 15.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
